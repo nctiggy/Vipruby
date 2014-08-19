30a44fdc2be155a52a272c6155560115910c66d0
Craig J Smith <nctiggy@gmail.com>
2014-08-18 20:03:20 -0700

    updated example

--- SBUX.rb
+++ SBUX.rb
@@ -7,6 +7,4 @@ vipr = Vipruby.new(config['base_url'],config['user_name'],config['password'])
 
 host = Host.new(type: 'other',name: 'test',fqdn: 'test.sbux.com',initiator_node: '10:13:27:65:60:38:68:BE',initiators_port: ['10:13:27:65:60:38:68:BD','10:13:27:65:60:38:68:BC'],protocol: 'fc',discoverable: 'false')
 
-vipr.add_host_and_initiators(host)
-
-vipr.host_exists?(host.name)
+vipr.add_host_and_initiators(host) unless vipr.host_exists?(host.name)
